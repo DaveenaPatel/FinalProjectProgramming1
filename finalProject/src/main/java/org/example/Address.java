28e1ec99b40e2977a6f6403b12008b88010452ee
DaveenaPatel <daveenampatel@gmail.com>
2024-05-25 20:16:09 -0400

    added isPostalCodeValid and test

--- finalProject/src/main/java/org/example/Address.java
+++ finalProject/src/main/java/org/example/Address.java
@@ -1,2 +1,43 @@
-package org.example;public class Address {
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+
+    /**
+     * Checks if postal code is valid
+     *
+     * @param postalCode the postalCode
+     * @return if postal code is valid true, if postal code not valid false
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode.length() == 6 || postalCode.length() == 7) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public Address(int streetNo, String street, String city, String province, String postalCode, String country) {
+        this.streetNo = streetNo;
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
 }
--- finalProject/src/main/java/org/example/MainTest.java
+++ finalProject/src/main/java/org/example/MainTest.java
@@ -1,2 +1,16 @@
-package org.example;public class MainTest {
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testIsPostalCodeValid() {
+        String postalCode = "123 456";
+        boolean expectedResult = true;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
